59b55f3207db1c0715b799426a4e56b76198e55a
Lee <110654347+BWLee97@users.noreply.github.com>
2024-07-22 18:56:52 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ def norm(x, xmin, xmax):
     return x
 
 
-with open('C:/Users/AdamN/Desktop/my_model.pkl', 'rb') as file:
+with open('BWLee97/IHCA/my_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 with st.expander('**预测结果**'):
